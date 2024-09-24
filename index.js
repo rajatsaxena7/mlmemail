d91cef65f3164ecb57503df636665712a7a2d891
Rajat saxena <62472831+rajatsaxena7@users.noreply.github.com>
2024-09-24 18:41:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://saiwealth.co.in", // Specify your domain here
+  methods: "POST",
+  allowedHeaders: ["Content-Type"],
+};
 
-app.use(cors(corsOptions));// Use CORS middleware to handle CORS
+// Ensure corsOptions is defined before using it
+app.use(cors(corsOptions)); // Use CORS middleware to handle CORS
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
